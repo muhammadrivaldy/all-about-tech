176be8e051c3d05f32dd6dff1acc5e24886963a1
Muhammad Rivaldy <muhammadrivaldy16@gmail.com>
2023-10-21 11:16:47 +0700

    Finish create an example of data class

--- code_smells/data_class/bad/main.go
+++ code_smells/data_class/bad/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import "fmt"
 
+type AreaOfCircle struct {
+	Phi    float32
+	Radius float32 // in cm
+}
+
+func main() {
+	object := AreaOfCircle{Phi: 3.14, Radius: 7}
+	fmt.Println(object.Phi * (object.Radius * object.Radius)) // Output: 153.86
 }
--- code_smells/data_class/good/main.go
+++ code_smells/data_class/good/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+type AreaOfCircle struct {
+	Phi    float32
+	Radius float32 // in cm
+}
+
+func (a AreaOfCircle) GetArea() float32 {
+	return a.Phi * (a.Radius * a.Radius)
+}
+
+func main() {
+	object := AreaOfCircle{Phi: 3.14, Radius: 7}
+	fmt.Println(object.GetArea()) // Output: 153.86
 }
