05c572051f86e2eaa76d3740fa8f0a0c3407cc64
Rivaldy SBS (Semangat Betul Saya) Was Here <muhammadrivaldy16@gmail.com>
2023-11-13 12:32:10 +0700

    Fix an example of polymorphism

--- oop_principles/polymorphism/polymorphism.ts
+++ oop_principles/polymorphism/polymorphism.ts
@@ -4,14 +4,23 @@ class User {
     }
 }
 
-class UserAdmin {
+class UserAdmin extends User {
     public get(): string {
         return "User admin object"
     }
 }
 
-class UserRegular {
+class UserRegular extends User {
     public get(): string {
         return "User regular object"
     }
 }
+
+var user = new User()
+console.log(user.get())
+
+var userAdmin = new UserAdmin()
+console.log(userAdmin.get())
+
+var userRegular = new UserRegular()
+console.log(userRegular.get())
